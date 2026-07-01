a5d5636ca079b6193efaa32fa286c02c360fe745
Ahmed-khattab <ahmedt.khattab96@gmail.com>
2026-07-01 22:42:31 +1000

    fix build-time secret key fallback

--- lovematch/settings.py
+++ lovematch/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-temporary-build-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
